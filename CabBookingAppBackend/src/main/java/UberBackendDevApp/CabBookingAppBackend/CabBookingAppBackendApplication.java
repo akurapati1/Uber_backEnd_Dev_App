e4a96f2246fe4242baa0b928e64ce8769b973290
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-01-01 09:26:26 -0600

    Initial commit with the spring dependencies and files

---
+++ CabBookingAppBackend/src/main/java/UberBackendDevApp/CabBookingAppBackend/CabBookingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package UberBackendDevApp.CabBookingAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBookingAppBackendApplication.class, args);
+	}
+
+}

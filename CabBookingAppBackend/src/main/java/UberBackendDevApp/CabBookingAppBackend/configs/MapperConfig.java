4956bd95162dea1a89eeba662e18bdcf3912d7b4
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-02-10 21:43:55 -0600

    partially Updated mapper config to convert point to pointDto and viceVersa

--- CabBookingAppBackend/src/main/java/UberBackendDevApp/CabBookingAppBackend/configs/MapperConfig.java
+++ CabBookingAppBackend/src/main/java/UberBackendDevApp/CabBookingAppBackend/configs/MapperConfig.java
@@ -1,16 +1,39 @@
 package UberBackendDevApp.CabBookingAppBackend.configs;
 
-
+import org.geolatte.geom.Point;
+import org.locationtech.jts.geom.Point;
+import UberBackendDevApp.CabBookingAppBackend.dto.PointDto;
+import UberBackendDevApp.CabBookingAppBackend.utils.GeometryUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.awt.*;
+
+import static UberBackendDevApp.CabBookingAppBackend.utils.GeometryUtil.*;
 
 @Configuration
 public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.typeMap(PointDto.class, Point.class).setConverter(context -> {
+            PointDto pointDto = context.getSource();
+            return GeometryUtil.createPoint(pointDto);
+        });
+
+        mapper.typeMap(Point.class, PointDto.class).setConverter(context -> {
+            Point point = context.getSource();
+            double coordinates[] = {
+                    point.getX(),
+                    point.getY()
+            };
+            return new PointDto(coordinates);
+        });
+
+        return mapper;
     }
 
 
